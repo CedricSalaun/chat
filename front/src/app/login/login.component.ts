e00dd0cd2b56afce5f933b1ff7ee0c163a88adcb
CedricSalaun <salaun.cedric@gmail.com>
2019-03-11 22:22:49 +0100

    login component wip

---
+++ front/src/app/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { LoginService } from '../service/login.service';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: [ './login.component.scss' ]
+})
+export class LoginComponent implements OnInit {
+    public loginForm: FormGroup;
+
+    public constructor(private readonly _fb: FormBuilder,
+                       private readonly _loginService: LoginService) {}
+
+    public ngOnInit(): void {
+        this.loginForm = this._fb.group({
+            user: [ { value: '', disabled: false }, Validators.required ],
+            password: [ { value: '', disabled: false }, Validators.required ]
+        });
+    }
+
+    public submitForm(): void {
+        this._loginService.login(this.loginForm.value).subscribe((response: Response) => {
+
+        });
+    }
+}
