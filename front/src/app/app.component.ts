2872da5130d45257d0eda4e53a8eea85f70e2fbe
CedricSalaun <salaun.cedric@gmail.com>
2019-03-12 23:03:13 +0100

    home html wip

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
     selector: 'app-root',
     template: `
-      <router-outlet #loadingElement></router-outlet>`,
-    styleUrls: [ './app.component.scss' ]
+      <router-outlet #loadingElement></router-outlet>`
 })
 export class AppComponent {
     @ViewChild('loadingElement') public readonly loadingElement: ElementRef;
--- front/src/app/service/login.service.ts
+++ front/src/app/service/login.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class LoginService {
 
-    public constructor(private _http: HttpClient) {}
+    public constructor(private readonly _http: HttpClient) {}
 
     public login($datas: any): Observable<Object> {
         const params: { password: string; login: string } = { password: $datas.password, login: $datas.login };
