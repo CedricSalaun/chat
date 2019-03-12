70810e5cd91d8fe17de586dd2067bdffef680cf7
CedricSalaun <salaun.cedric@gmail.com>
2019-03-12 22:40:50 +0100

    guard pb jb_tmp

--- front/src/app/Components/login/login.component.ts
+++ front/src/app/Components/login/login.component.ts
@@ -23,8 +23,9 @@ export class LoginComponent implements OnInit {
     }
 
     public submitForm(): void {
-        this._loginService.login(this.loginForm.value).pipe(first()).subscribe((response: any) => {
-            localStorage.setItem('chat_key', response);
-        });
+        this._loginService.login(this.loginForm.value).pipe(first()).subscribe(
+            (response: any) => localStorage.setItem('chat_key', response),
+            (error: any) => console.error({ error })
+        );
     }
 }
--- front/src/app/app.routing.ts
+++ front/src/app/app.routing.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginGuard } from './guards/login.guard';
 import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', canActivate: [ LoginGuard ], component: LoginComponent },
     { path: 'home', canActivate: [ LoginGuard ], component: HomeComponent },
     { path: '**', redirectTo: 'login' }
 ];
